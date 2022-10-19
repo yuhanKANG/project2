60bb302d9d3f3f29114a50cca33d060c17b3425c
yuhanKANG <overlight1@gmail.com>
2022-10-19 16:54:41 +0900

    pj2

--- single_pages/models.py
+++ single_pages/models.py
@@ -1,3 +1,12 @@
+import pwd
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
