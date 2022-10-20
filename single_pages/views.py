256a0722b04e72f8db5607272a0a937460e6cfab
yuhanKANG <overlight1@gmail.com>
2022-10-20 15:38:34 +0900

    update

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class Postlist(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+    
 # def index(request):
 #     return render(
 #         request,
